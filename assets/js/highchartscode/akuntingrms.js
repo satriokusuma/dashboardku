526d38e551920c4939204c726be3a4988d832b19
satriokusuma.com <sir.poteto@gmail.com>
2021-01-04 01:51:39 +0700

    Update Page Akunting dll

---
+++ assets/js/highchartscode/akuntingrms.js
@@ -0,0 +1,185 @@
+Highcharts.setOptions({
+    colors: ['#b8de6f','#9ba4b4']
+});
+  var Total = 0;
+  var chart_rms = new Highcharts.chart({
+        chart: {
+            renderTo: 'chart-pie-akuntingrms',
+            backgroundColor: null,
+            plotBackgroundColor: null,
+            plotBorderWidth: null,
+            plotShadow: false,
+            type: 'pie',
+            height: 220,
+        },
+        tooltip: {
+            followTouchMove: true,
+            useHTML: true,
+        },   
+        title: {
+            text: ''
+        },
+        credits: {
+        enabled: false
+        },
+        plotOptions: {
+            pie: {
+                allowPointSelect: true,
+                cursor: 'pointer',
+                dataLabels: {
+                    enabled: false
+                },
+                showInLegend: true
+            }
+        },
+       legend: {
+            useHTML: true,
+            labelFormatter: function() {
+                Total += this.y;
+                return '<div><span style="color:#fff;">'+ this.name +' : '+'Rp.'+'</span><span style="color:#fff;">'+ this.y +'</span></div>';
+            },
+            itemStyle: {
+                fontSize: '11px',
+                fontWeight: 500,
+                whiteSpace: 'nowrap',
+              },
+            align: 'left',
+            verticalAlign: 'bottom',
+            layout: 'horizontal',
+            symbolWidth: 10,
+            symbolHeight: 10,
+           
+        },
+        series: [{
+            name: 'Total',
+            colorByPoint: true,
+            // innerSize: '50%',
+            data: [{
+                name: 'Profit',
+                y: 300000000,
+                
+               
+            }, {
+                name: 'Loss',
+                y: 100000000,
+                sliced: true,
+                selected: true,
+               
+            }
+            ]
+        }],
+        navigation: {
+            buttonOptions: {
+                verticalAlign: 'top',
+                align: 'left',
+            }
+        },
+        exporting: {
+            buttons: {
+                contextButton: {
+                    menuItems: ['downloadXLS','viewData','downloadPDF']
+                }
+            }
+        } 
+});
+
+Highcharts.setOptions({
+    colors: ['#54e346', '#4baee3']
+});
+
+var chart_area = new Highcharts.chart({
+        chart: {
+            renderTo: 'area-akuntingRMS', 
+            backgroundColor: null,
+            type: 'area' 
+        },
+        xAxis: {
+            categories: ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
+            labels: {
+            style: {
+            fontSize: '12px',
+            color: '#43425D',
+            fontWeight:'bold'
+            }
+            }, 
+            title: {
+                text: null
+            },
+            min: 5,
+            max:11,
+            scrollbar: {
+            enabled: true,
+            barBackgroundColor: '#dbdbdb',
+            barBorderRadius: 7,
+            barBorderWidth: 0,
+            buttonBackgroundColor: '#dbdbdb',
+            buttonBorderWidth: 0,
+            buttonArrowColor: 'black',
+            buttonBorderRadius: 7,
+            rifleColor: 'black',
+            trackBackgroundColor: '#ebebeb',
+            trackBorderWidth: 1,
+            trackBorderColor: 'silver',
+            trackBorderRadius: 7,
+            size: 8
+            },
+        },
+        yAxis: {
+            min:0,
+            title: false, 
+        },
+        title: {
+            text: 'Division'
+        },
+        credits: {
+        enabled: false
+        },
+        tooltip: {
+            formatter: function () {
+            return '<b>' + this.x + '</b><br/>' +
+            '<b>'+ 'Total :' +'</b>'+' '+' ' +'<b>'+'Rp. '+ this.y + '</b>'+'<br/>'
+            }
+        },
+      
+        plotOptions: {
+            series: {
+                label: {
+                connectorAllowed: false
+                },
+            },
+            line: {
+                cursor: 'pointer',
+                showInLegend: true,
+            }
+        },
+
+        series:[{
+            name: 'Profit',
+            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175, 119931, 167133, 154175,200000]
+        }, {
+            name: 'Loss',
+            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434, 219931, 187133, 195555,50000]
+        }, 
+        ],
+    
+        navigation: {
+            buttonOptions: {
+                verticalAlign: 'top',
+            }
+        },
+        exporting: {
+            sourceWidth: 1200,
+            buttons: {
+                contextButton: {
+                    menuItems: ['downloadXLS','viewData','downloadPDF']
+                }
+            },
+            chartOptions: {
+                xAxis: [{
+                    categories: ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
+                }]
+            }
+        }  
+    });
+
+
--- assets/js/highchartscode/chart.js
+++ assets/js/highchartscode/chart.js
@@ -4,7 +4,7 @@ Highcharts.setOptions({
   var Total = 0;
   var chart_zone = new Highcharts.chart({
         chart: {
-            renderTo: 'chart-pie1',
+            renderTo: 'chart-pie-profitdiv',
             backgroundColor: null,
             plotBackgroundColor: null,
             plotBorderWidth: null,
@@ -50,17 +50,17 @@ Highcharts.setOptions({
            
         },
         series: [{
-            name: 'Zone',
+            name: 'Profit',
             colorByPoint: true,
             // innerSize: '50%',
             data: [{
                 name: 'RMS',
-                y: 500000000,
+                y: 40000000,
                 sliced: true,
                 selected: true,
             }, {
                 name: 'IFS',
-                y: 300000000,
+                y: 30000000,
             }]
         }],
         navigation: {
@@ -82,7 +82,7 @@ Highcharts.setOptions({
   var Total = 0;
   var chart_playzone = new Highcharts.chart({
         chart: {
-            renderTo: 'chart-pie2',
+            renderTo: 'chart-pie-omzetdiv',
             backgroundColor: null,
             plotBackgroundColor: null,
             plotBorderWidth: null,
@@ -113,7 +113,7 @@ Highcharts.setOptions({
             useHTML: true,
             labelFormatter: function() {
                 Total += this.y;
-                return '<div><span>'+ this.name +' : '+'Rp.'+'</span><span style="color:#43425D;font-weight: 700;">'+ this.y +'</span></div>';
+                return '<div><span>'+ this.name +' : '+'</span><span style="color:#43425D;font-weight: 700;">'+ this.y +'</span></div>';
             },
             itemStyle: {
                 fontSize: '11.5px',
@@ -125,7 +125,7 @@ Highcharts.setOptions({
             layout: 'horizontal',
         },
         series: [{
-            name: 'Kiddie Land',
+            name: 'Omzet',
             colorByPoint: true,
             data: [{
                 name: 'RMS',
@@ -152,10 +152,13 @@ Highcharts.setOptions({
         } 
 });
 
-var Total = 0;
+    Highcharts.setOptions({
+        colors: ['#fdb827','#b8de6f','#ffabe1']
+    });
+  var Total = 0;
   var chart_kiddieland = new Highcharts.chart({
         chart: {
-            renderTo: 'chart-pie3',
+            renderTo: 'chart-pie-profitproducts',
             backgroundColor: null,
             plotBackgroundColor: null,
             plotBorderWidth: null,
@@ -190,21 +193,35 @@ var Total = 0;
                 return '<div><span style="color:#fff;">'+ this.name +' : '+'</span><span style="color:#fff;font-weight: 400;">'+ this.y + '</span></div>';
 
             },
+            itemStyle: {
+                fontSize: '11.5px',
+                fontWeight: 700,
+                whiteSpace: 'nowrap',
+              },
+            align: 'left',
+            verticalAlign: 'bottom',
+            layout: 'horizontal',
         },
         series: [{
-            name: 'Status',
+            name: 'Profit',
             colorByPoint: true,
             data: [{
-                name: 'RMS',
-                y: 500000,
-                sliced: true,
-                selected: true,
+                name: 'PlayZone',
+                y: 400000000,
+              
                 
             }, {
-                name: 'IFS',
-                y: 800000,
+                name: 'Zone',
+                y: 700000000,
                
-            }]
+            },{
+                name: 'Kiddie',
+                y: 200000000,
+                sliced: true,
+                selected: true,
+               
+            }
+            ]
         }],
         navigation: {
             buttonOptions: {
@@ -222,12 +239,197 @@ var Total = 0;
 });
 
 Highcharts.setOptions({
+    colors: ['#fdb827','#b8de6f','#ffabe1']
+});
+var Total = 0;
+var chart_kiddieland = new Highcharts.chart({
+    chart: {
+        renderTo: 'chart-pie-omzetproducts',
+        backgroundColor: null,
+        plotBackgroundColor: null,
+        plotBorderWidth: null,
+        plotShadow: false,
+        type: 'pie',
+        height: 220,
+    },
+    tooltip: {
+        followTouchMove: true
+      },
+    title: {
+        text: ''
+    },
+    credits: {
+    enabled: false
+    },
+    plotOptions: {
+        pie: {
+            allowPointSelect: true,
+            cursor: 'pointer',
+            dataLabels: {
+                enabled: false
+            },
+            showInLegend: true,
+            legendColor:'#fff',
+        }
+    },
+   legend: {
+        useHTML: true,
+        labelFormatter: function() {
+            Total += this.y;
+            return '<div><span>'+ this.name +' : '+'</span><span style="font-weight: 600;">'+ this.y + '</span></div>';
+
+        },
+        itemStyle: {
+            fontSize: '11.5px',
+            fontWeight: 700,
+            whiteSpace: 'nowrap',
+          },
+        align: 'left',
+        verticalAlign: 'bottom',
+        layout: 'horizontal',
+    },
+    series: [{
+        name: 'Omzet',
+        colorByPoint: true,
+        data: [{
+            name: 'PlayZone',
+            y: 400000000,
+           
+            
+        }, {
+            name: 'Zone',
+            y: 700000000,
+           
+        },{
+            name: 'Kiddie',
+            y: 200000000,
+            sliced: true,
+            selected: true,
+           
+        }
+        ]
+    }],
+    navigation: {
+        buttonOptions: {
+            verticalAlign: 'top',
+            align: 'left',
+        }
+    },
+    exporting: {
+        buttons: {
+            contextButton: {
+                menuItems: ['downloadXLS','viewData']
+            }
+        }
+    } 
+});
+
+Highcharts.setOptions({
     colors: ['#54e346', '#4baee3']
 });
 
 var chart_line = new Highcharts.chart({
         chart: {
-            renderTo: 'line-1', 
+            renderTo: 'line-profitDiv', 
+            backgroundColor: null,
+            type: 'area' 
+        },
+        xAxis: {
+            categories: ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
+            labels: {
+            style: {
+            fontSize: '12px',
+            color: '#43425D',
+            fontWeight:'bold'
+            }
+            }, 
+            title: {
+                text: null
+            },
+            min: 5,
+            max:11,
+            scrollbar: {
+            enabled: true,
+            barBackgroundColor: '#dbdbdb',
+            barBorderRadius: 7,
+            barBorderWidth: 0,
+            buttonBackgroundColor: '#dbdbdb',
+            buttonBorderWidth: 0,
+            buttonArrowColor: 'black',
+            buttonBorderRadius: 7,
+            rifleColor: 'black',
+            trackBackgroundColor: '#ebebeb',
+            trackBorderWidth: 1,
+            trackBorderColor: 'silver',
+            trackBorderRadius: 7,
+            size: 8
+            },
+        },
+        yAxis: {
+            min:0,
+            title: false, 
+        },
+        title: {
+            text: 'Division'
+        },
+        credits: {
+        enabled: false
+        },
+        tooltip: {
+            formatter: function () {
+            return '<b>' + this.x + '</b><br/>' +
+            '<b>'+ 'Total :' +'</b>'+' '+' ' +'<b>'+'Rp. '+ this.y + '</b>'+'<br/>'
+            }
+        },
+      
+        plotOptions: {
+            series: {
+                label: {
+                connectorAllowed: false
+                },
+            },
+            line: {
+                cursor: 'pointer',
+                showInLegend: true,
+            }
+        },
+
+        series:[{
+            name: 'RMS',
+            data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175, 119931, 167133, 154175,200000]
+        }, {
+            name: 'IFS',
+            data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434, 219931, 187133, 195555,50000]
+        }, 
+        ],
+    
+        navigation: {
+            buttonOptions: {
+                verticalAlign: 'top',
+            }
+        },
+        exporting: {
+            sourceWidth: 1200,
+            buttons: {
+                contextButton: {
+                    menuItems: ['downloadXLS','viewData','downloadPDF']
+                }
+            },
+            chartOptions: {
+                xAxis: [{
+                    categories: ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
+                }]
+            }
+        }  
+    });
+
+Highcharts.setOptions({
+    colors: ['#54e346', '#4baee3']
+});
+
+var chart_line = new Highcharts.chart({
+        chart: {
+            renderTo: 'line-omzetDiv', 
             backgroundColor: null,
             type: 'line' 
         },
@@ -326,7 +528,109 @@ var chart_line = new Highcharts.chart({
     
     var chart_lineProduct = new Highcharts.chart({
             chart: {
-                renderTo: 'line-product', 
+                renderTo: 'line-profitProducts', 
+                backgroundColor: null,
+                type: 'area' 
+            },
+            xAxis: {
+                categories: ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
+                labels: {
+                style: {
+                fontSize: '12px',
+                color: '#43425D',
+                fontWeight:'bold'
+                }
+                }, 
+                title: {
+                    text: null
+                },
+                min: 5,
+                max:11,
+                scrollbar: {
+                enabled: true,
+                barBackgroundColor: '#dbdbdb',
+                barBorderRadius: 7,
+                barBorderWidth: 0,
+                buttonBackgroundColor: '#dbdbdb',
+                buttonBorderWidth: 0,
+                buttonArrowColor: 'black',
+                buttonBorderRadius: 7,
+                rifleColor: 'black',
+                trackBackgroundColor: '#ebebeb',
+                trackBorderWidth: 1,
+                trackBorderColor: 'silver',
+                trackBorderRadius: 7,
+                size: 8
+                },
+            },
+            yAxis: {
+                min:0,
+                title: false, 
+            },
+            title: {
+                text: 'Products'
+            },
+            credits: {
+            enabled: false
+            },
+            tooltip: {
+                formatter: function () {
+                return '<b>' + this.x + '</b><br/>' +
+                '<b>'+ 'Total :' +'</b>'+' '+' ' +'<b>'+'Rp. '+ this.y + '</b>'+'<br/>'
+                }
+            },
+          
+            plotOptions: {
+                series: {
+                    label: {
+                    connectorAllowed: false
+                    },
+                },
+                line: {
+                    cursor: 'pointer',
+                    showInLegend: true,
+                }
+            },
+    
+            series:[{
+                name: 'Play Zone',
+                data: [43934, 52503, 57177, 69658, 97031, 119931, 137133, 154175, 119931, 167133, 154175,200000]
+            }, {
+                name: 'Zone 2000',
+                data: [24916, 24064, 29742, 29851, 32490, 30282, 38121, 40434, 219931, 187133, 195555,50000]
+            }, {
+                name: 'Kiddie Land',
+                data: [11744, 17722, 16005, 19771, 20185, 24377, 32147, 39387, 19931, 127133, 15000,80000]
+            }
+            ],
+        
+            navigation: {
+                buttonOptions: {
+                    verticalAlign: 'top',
+                }
+            },
+            exporting: {
+                sourceWidth: 1200,
+                buttons: {
+                    contextButton: {
+                        menuItems: ['downloadXLS','viewData','downloadPDF']
+                    }
+                },
+                chartOptions: {
+                    xAxis: [{
+                        categories: ['2009', '2010', '2011', '2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020'],
+                    }]
+                }
+            }  
+        });
+
+    Highcharts.setOptions({
+        colors: ['#fdb827','#b8de6f','#ffabe1']
+    });
+    
+    var chart_lineProduct = new Highcharts.chart({
+            chart: {
+                renderTo: 'line-omzetproduct', 
                 backgroundColor: null,
                 type: 'line' 
             },
@@ -425,16 +729,14 @@ var chart_line = new Highcharts.chart({
     Highcharts.setOptions({
 		colors: ['#b8de6f','#888888']
     });
-    let dataZone = [
+    let dataCompareProducts = [
         [23934, 22503, 27177, 69658, 27031, 19931, 37133, 54175, 19931, 167133, 154175,20000], 
-        [24916, 24064, 29742, 79851, 32490, 30282, 38121, 40434, 219931, 187133, 195555,50000]
+        [21550, 21060, 25742, 59851, 12490, 10282, 28121, 30434, 219931, 187133, 15555,10000]
     ];
-    function formatterZone(baseSeries) {
-        return Highcharts.numberFormat(this.y / dataZone[baseSeries][this.point.index] * 100,0) + '%'
-    }
+  
     var chart_bar = new Highcharts.chart({
         chart: {
-            renderTo: 'line-2',
+            renderTo: 'line-Products',
             type: 'bar',
             backgroundColor: null,   
         },
@@ -476,7 +778,7 @@ var chart_line = new Highcharts.chart({
             title: false,   
         },
         title: {
-            text: 'Omset & Target'
+            text: 'Years & Months'
         },
         credits: {
         enabled: false
@@ -511,19 +813,15 @@ var chart_line = new Highcharts.chart({
 
         series:[
             {
-            name:'Omset Sale',
-            data: dataZone[0],
+            name:'Income',
+            data: dataCompareProducts[0],
             dataLabels: {
-                align: 'left',
-                zIndex: 1,
-                formatter: function() {
-                  return formatterZone.call(this, 1);
-                }
-              }
+                enabled:false
+               }
             },
             {
-            name:'Target Sale',
-            data: dataZone[1],
+            name:'Outcome',
+            data: dataCompareProducts[1],
             dataLabels: {
                enabled:false
               }
