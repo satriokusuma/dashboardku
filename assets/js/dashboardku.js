0f8abc6fa7483b2d254657f5838d14baca7773c4
satriokusuma.com <sir.poteto@gmail.com>
2021-01-06 01:00:47 +0700

    Update sidebar

--- assets/js/dashboardku.js
+++ assets/js/dashboardku.js
@@ -1,17 +1,17 @@
 
 
- (function(){
-     isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
+//  (function(){
+//      isWindows = navigator.platform.indexOf('Win') > -1 ? true : false;
 
-     if (isWindows && !$('div').hasClass('sidebar-mini')){
-        // if we are on windows OS we activate the perfectScrollbar function
-        $('.sidebar .sidebar-wrapper').perfectScrollbar();
+//      if (isWindows && !$('div').hasClass('sidebar-mini')){
+//         // if we are on windows OS we activate the perfectScrollbar function
+//         $('.sidebar .sidebar-wrapper').perfectScrollbar();
 
-        $('html').addClass('perfect-scrollbar-on');
-    } else {
-        $('html').addClass('perfect-scrollbar-off');
-    }
- })();
+//         $('body').addClass('perfect-scrollbar-on');
+//     } else {
+//         $('body').addClass('perfect-scrollbar-off');
+//     }
+//  })();
 
 var breakCards = true;
 
